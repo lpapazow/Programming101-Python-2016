00f1f6ddb64cf45771fcb1554b679531331b146f
lpapazow <lpapazow@gmail.com>
2017-01-10 18:00:53 +0200

    Update sql_manage_queries.py

--- week11/Money-In-The-Bank/sql_manage_queries.py
+++ week11/Money-In-The-Bank/sql_manage_queries.py
@@ -3,7 +3,8 @@ CREATE_TABLE = '''CREATE TABLE IF NOT EXISTS
                 username TEXT,
                 password TEXT,
                 balance REAL DEFAULT 0,
-                message TEXT)'''
+                message TEXT
+                email TEXT)'''
 
 CHANGE_CLIENT_MESSAGE = '''UPDATE clients SET message = ? WHERE id = ?'''
   
@@ -12,6 +13,11 @@ CHANGE_CLIENT_PASS = '''UPDATE clients SET password = ? WHERE id = ?'''
 REGISTER_USER = '''INSERT INTO CLIENTS (username, password) 
       VALUES (?, ?)'''
 
-SELECT_USER = '''SELECT id, username, balance, message 
+SELECT_USER = '''SELECT id, username, balance, message, email 
                 FROM clients 
                 WHERE username = ? AND password = ? LIMIT 1'''
+
+SELECT_USER_EMAIL = '''SELECT email 
+                FROM clients 
+                WHERE username = ?
+                LIMIT 1'''
