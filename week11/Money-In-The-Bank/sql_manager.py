1f202c680c88df2be74e961334bad166297a53f7
lpapazow <lpapazow@gmail.com>
2017-01-10 14:12:01 +0200

    Update sql_manager.py

--- week11/Money-In-The-Bank/sql_manager.py
+++ week11/Money-In-The-Bank/sql_manager.py
@@ -7,39 +7,27 @@ cursor = conn.cursor()
 
 
 def create_clients_table():
-    create_query = '''create table if not exists
-        clients(id INTEGER PRIMARY KEY AUTOINCREMENT,
-                username TEXT,
-                password TEXT,
-                balance REAL DEFAULT 0,
-                message TEXT)'''
-
-    cursor.execute(create_query)
+    cursor.execute(sql_manage_queries.CREATE_TABLE)
 
 
 def change_message(new_message, logged_user):
-    update_sql = "UPDATE clients SET message = '%s' WHERE id = '%s'" % (new_message, logged_user.get_id())
-    cursor.execute(update_sql)
+    cursor.execute(sql_manage_queries.CHANGE_CLIENT_MESSAGE, (new_message, logged_user.get_id()))
     conn.commit()
     logged_user.set_message(new_message)
-
+    
 
 def change_pass(new_pass, logged_user):
-    update_sql = "UPDATE clients SET password = '%s' WHERE id = '%s'" % (new_pass, logged_user.get_id())
-    cursor.execute(update_sql)
+    cursor.execute(sql_manage_queries.CHANGE_CLIENT_PASS, (new_pass, logged_user.get_id()))
     conn.commit()
 
 
 def register(username, password):
-    insert_sql = "insert into clients (username, password) values ('%s', '%s')" % (username, password)
-    cursor.execute(insert_sql)
+    cursor.execute(sql_manage_queries.REGISTER_USER, (username, password))
     conn.commit()
 
 
 def login(username, password):
-    select_query = "SELECT id, username, balance, message FROM clients WHERE username = '%s' AND password = '%s' LIMIT 1" % (username, password)
-
-    cursor.execute(select_query)
+    cursor.execute(sql_manage_queries.SELECT_USER, (username, password))
     user = cursor.fetchone()
 
     if(user):
